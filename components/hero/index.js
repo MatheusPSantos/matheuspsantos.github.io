8159e55e26a1e1e974468120f1bb90009b40fef2
Matheus Pereira dos Santos <msantos.2015@alunos.utfpr.edu.br>
2021-03-16 00:42:56 -0300

    create hero component

---
+++ components/hero/index.js
@@ -0,0 +1,13 @@
+
+function Hero() {
+  return (
+    <section className="start">
+      <header className="header container wow fadeIn">
+        <h2>Matheus Pereira dos Santos</h2>
+        <h1>Fullstack Developer</h1>
+      </header>
+    </section>
+  )
+}
+
+export default Hero;
