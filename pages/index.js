799cd8fe9fcf889c73cb9bc62ea63cf489d5f594
Matheus Pereira dos Santos <msantos.2015@alunos.utfpr.edu.br>
2021-03-22 00:54:47 -0300

    impv/add projects component

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import About from '../components/about';
 import Contact from '../components/contact';
 import Hero from '../components/hero';
+import MyProjects from '../components/my-projects';
 import Navbar from '../components/navbar';
 
 export default function HomePage() {
@@ -13,27 +14,7 @@ export default function HomePage() {
       <div className="row"></div>
       <About />
       <div className="row"></div>
-      <section className="my-projects wow fadeIn" data-wow-duration="2s" data-wow-delay="0.5s" id="my-projects">
-        <div className="container">
-          <h2>My projects</h2>
-          <h3>Here are my projects, both personal and professional.</h3>
-          {/* <!-- Cards --> */}
-          <div className="card-projects">
-            <div className="row">
-              <div className="card col-md-3" style={{ textAlign: "left", padding: "0px", marginLeft: "5px" }}>
-                <img src="/unectjr.png" alt="A company formed by students" className="card-img-top" />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    Unect Jr.
-                      </h5>
-                  <p className="card-text">Unect Jr. it's a technology company formed by UTFPR-CP students.</p>
-                  <a href="http://www.unect.com.br/" target="_blank" className="btn btn-primary">Read More</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <MyProjects />
       <div className="row"></div>
       <Contact />
 
