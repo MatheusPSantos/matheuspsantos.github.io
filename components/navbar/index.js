21a6f3485926fb2727254b280df7666671ae8420
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-04-24 00:56:39 -0300

    remove netlify cms and update css

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-purple fixed-top">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-black fixed-top">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
