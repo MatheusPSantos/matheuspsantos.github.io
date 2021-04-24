d898a8dbdcf35989cb10bbf7b48bd00b217c2944
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-04-24 00:57:46 -0300

    update navbar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-black fixed-top">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-navbar fixed-top">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
