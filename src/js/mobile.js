67ebe0f368402f6eae6958f8ca1c3009b1f85bbf
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-07-29 00:06:33 -0300

    fazendo o burguer menu funcionar

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -2,15 +2,24 @@ const $doc = (el) => document.querySelector(el);
 // closing and open nav-left
 const closeMenuButton = $doc("#close-menu");
 const navLeft = $doc(".nav-left");
+const burguerButton = $doc("#burguer-button");
 
 closeMenuButton.addEventListener("click", (e) => {
     e.preventDefault();
     
-    if(!navLeft.classList.toString().includes("nav-left-disabled")) {
-        navLeft.classList.remove("nav-left");
+    if(navLeft.classList.toString().includes("nav-left-enabled")) {
+        navLeft.classList.remove("nav-left-enabled");
         navLeft.classList.remove("container");
-        navLeft.classList.add("nav-left-disabled");
-        navLeft.classList.add("container");
+        navLeft.classList.add("nav-left");
     }
 
 });
+
+burguerButton.addEventListener("click", e => {
+    e.preventDefault();
+    if(!navLeft.classList.toString().includes("nav-left-enabled")) {
+        navLeft.classList.remove("nav-left");
+        navLeft.classList.add("nav-left-enabled");
+        navLeft.classList.add("container");
+    }
+})
